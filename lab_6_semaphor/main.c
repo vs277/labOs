8e3f2e6eb0ee124453198d92add5dbba657888a2
vs277 <vs277@yandex.ru>
2018-12-17 21:25:06 +0300

    fixed 2.0

--- lab_6_semaphor/main.c
+++ lab_6_semaphor/main.c
@@ -10,18 +10,12 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
-//union semun {
-//  int              val;    /* Value for SETVAL */
-//  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-//  unsigned short  *array;  /* Array for GETALL, SETALL */
-//  struct seminfo  *__buf;  /* Buffer for IPC_INFO
-//                              (Linux specific) */
-//};
 
 
 int main(){
-	struct sembuf do_it = { 0, 1, 0 };
-	struct sembuf wait_w = { 0, -1, 0 };
+	struct sembuf write_it = { 0, 1 , 0 };
+	struct sembuf read_it = { 0, -1 , 0 };
+	struct sembuf wait_it ={ 0 , 0 , 0 };
 	key_t key;
 	if((key=ftok("./file",'B'))<0){
 		printf("error ftok\n");
@@ -35,64 +29,45 @@ int main(){
 	if ((semid = semget(key, 1,0666 | IPC_CREAT)) < 0) {
 		printf("error sem \n");
 		exit(0);
-	}else	printf("semafor created \n");
-	union semun sem_union;
-	sem_union.val = 1;
-	if (semctl(semid, 0, SETVAL, sem_union)==-1) printf("error value sem \n");
+	}else	printf("semafor created %d \n",semid);
 
 	if (pipe(file_pipes) == 0){
 		if((k=fork())==0){
 			int i=0;
 			while(i<10){
-				if (semop(semid, &wait_w, 1) < 0) {
-					printf("Невозможно выполнить операциюn");
-					exit(1);
-				}
-
+				semop(semid,&wait_it,1);
 				It=time(NULL);
 				char* thattime=asctime(localtime(&It));
 				write(file_pipes[1],thattime ,strlen(thattime));
 				printf("sem is my and i write my time for pr1\n");
 				i++;
-				if (semop(semid, &do_it, 1) < 0) {
-					printf("Невозможно выполнить операциюn");
-					exit(1);
-				}	
+				semop(semid,&write_it,1);
 				sleep(1);
 			}
-			semctl(semid, 0, IPC_RMID);
+			semctl(semid,IPC_RMID,0);
 			exit(EXIT_SUCCESS);
 		}
 		else {
-			sleep(1);
+                 
 			char times[25];
 			while(1){
+
 				if(k==(waitpid(k,0,WNOHANG))){
 					printf("process %d died,i exit\n",k);
 					exit(EXIT_SUCCESS);
 				}
+				if((waitpid(k,0,WNOHANG))<0){
+                                        printf("process %d died,i exit\n",k);
+                                        exit(EXIT_SUCCESS);
+                                }
 
-				if (semop(semid, &wait_w, 1) < 0) {
-					printf("erorror \n");
-					if(k==(waitpid(k,0,WNOHANG))){
-						printf("process %d died,i exit\n",k);
-						exit(EXIT_SUCCESS);
-					}
-
-					exit(1);;
+				if((semop(semid,&read_it,1))<0) {
+				printf("semofor denied,proces 2 died,i am go to\n");
+				exit(EXIT_SUCCESS);
 				}
 				read(file_pipes[0],times,sizeof(times));
 				printf("time is 2 process %s",times);
-				sleep(1);
-				if (semop(semid, &do_it, 1) < 0) {
-					printf("eroro wait m.b. semafor was deleted pr2\n");
-					if(k==(waitpid(k,0,WNOHANG))){
-						printf("process %d died,i exit\n",k);
-						exit(EXIT_SUCCESS);
-					}
-
-					exit(0);
-				}
+				//sleep(1);
 
 			}
 
